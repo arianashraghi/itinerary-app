c0f4cbc4778b5944e2ee124174c258c676d353b6
Arian Ashraghi <arian.ashraghi@gmail.com>
2022-09-05 19:51:55 -0700

    Configures app.js

--- api/app.js
+++ api/app.js
@@ -1,3 +1,11 @@
 const Express = require("express");
 const Http = require("http");
 const bodyParser = require("body-parser");
+
+const app = Express();
+const server = Http.createServer(app);
+
+// Parse requests
+app.use(bodyParser.json());
+
+server.listen(80);
