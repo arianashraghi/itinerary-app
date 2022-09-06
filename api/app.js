b6775996b3361d5d88e6d54fb682d7576010a60e
Arian Ashraghi <arian.ashraghi@gmail.com>
2022-09-06 13:21:49 -0700

    Adds cors middleware

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 const Express = require("express");
 const Http = require("http");
 const bodyParser = require("body-parser");
+const corsMiddleware = require("./middlewares/cors");
 // Setup Database
 const database = require("./util/database");
 
@@ -12,8 +13,11 @@ const server = Http.createServer(app);
 
 app.use("/", Express.static("public"));
 
+// Middlewares
 // Parse requests
 app.use(bodyParser.json());
+// Handles cors
+app.use(corsMiddleware);
 
 app.use(itineraryRouter);
 
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,7 +1,28 @@
+import { useEffect } from "react";
 import "./App.css";
 import Layout from "./components/Layout/Layout";
 
 function App() {
+  useEffect(() => {
+    fetch("http://localhost/itinerary", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        alert(data);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  }, []);
+
   return (
     <Layout>
       <div className="App">
