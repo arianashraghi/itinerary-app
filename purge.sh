b52e21fb2dd4a460b4aa4bd35d0a4224ebbd416d
Arian Ashraghi <arian.ashraghi@gmail.com>
2022-09-05 20:27:28 -0700

    Removed docker files

--- purge.sh
+++
@@ -1,9 +0,0 @@
-# WARNING IT WILL REMOVE ALL IMAGES, CONTAINERS AND VOLUMES
-
-#Delete all containers using the following command:
-docker rm -f $(docker ps -a -q) 
-#Delete all volumes using the following command:
-docker volume rm $(docker volume ls -q)
-#Delete all images
-docker rmi -f $(docker images -q)
-#Restart the containers using the following command:
